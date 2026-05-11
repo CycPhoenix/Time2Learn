8ecfc15114a76e27d88019ba0432495785daf426
CycPhoenix <63090086+CycPhoenix@users.noreply.github.com>
2026-05-11 13:39:58 +0800

    Create Logout.aspx.cs

---
+++ Time2Learn/Logout.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Time2Learn.App_Code;
+
+namespace Time2Learn
+{
+    public partial class Logout : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            AuthHelper.ClearSession();
+            Response.Redirect("Login.aspx");
+        }
+    }
+}
